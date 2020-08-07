c4dcf432933c2764796dd7852bbb013b0e0bccc1
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-08-08 00:29:26 +0900

    昔懐かしMarqueeっぽいのを入れた

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <p>
-      サンプル
+<div>
+<myheader></myheader>
+<div>
+    <p class="basetext" v-if="msg.length > 0">
+      {{msg}}
     </p>
+    <p v-else>
+      no text
+    </p>
+</div>
   </div>
 </template>
+
+<script>
+import myheader from './components/myheader'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      msg: 'ちんちん！'
+    }
+  }
+}
+</script>
+<style>
+div{
+  text-align: center;
+}
+</style>
---
+++ src/components/myheader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+<p id="marquee">
+{{msg}}
+</p>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#marquee',
+  data () {
+    return {
+      msg: 'Welcome to my homepage!!                          '
+    }
+  },
+  updated () {
+    setTimeout(() => {
+      this.msg = this.msg.slice(-2) + this.msg.slice(0, -2)
+    }
+      , 1000
+    )
+  },
+  created () {
+    setTimeout(() => {
+      this.msg = this.msg.slice(-2) + this.msg.slice(0, -2)
+    }
+      , 1000
+    )
+  }
+}
+</script>
+
+<style>
+#marquee{
+  background-color: #ffff00 ;
+  display: inline ;
+  white-space: pre ;
+}
+</style>
